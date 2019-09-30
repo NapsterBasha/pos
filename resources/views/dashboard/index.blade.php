a2b6e2d24aa1e51750043961f1a4755d4925021c
mahmoudbasha <mahmoud.basha2017@outlook.com>
2019-09-30 11:16:47 +0200

    initial commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+
+    <diV class="content-wrapper">
+        <section class="content-header">
+            <h1>Blank Page
+                <small>it is all starts here</small>
+            </h1>
+
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
+            </ol>
+        </section>
+        <section class="content">
+            <h1>This is dashboard</h1>
+        </section> <!-- end of content !-->
+
+    </diV><!-- end of wrapper !-->
+
+@endsection
